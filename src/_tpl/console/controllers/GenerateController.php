40c5a8eeeba1222772eaa2cf1ed6c7627fe689de
and_y87 <kidin_a@list.ru>
2019-11-04 18:42:16 +0300

    Add TPL dir + files

---
+++ src/_tpl/console/controllers/GenerateController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace console\controllers;
+
+use andy87\yii2\generator\console\controllers\GeneratorController as Source;
+
+/**
+ * Class GenerateController
+ *
+ *      Controller
+ *
+ * @package console\controllers
+ */
+class GenerateController extends Source
+{
+    public $config = [
+        'model'         => (object) [
+            'modelClass'            => '#TableName#',
+            'ns'                    => "common\\models\\items\\source",
+            'baseClass'             => "common\\models\\core\\BaseModel"
+        ],
+        'crud'          => (object) [
+            'modelClass'            => "common\\models\\items\\source\\#TableName#",
+            'viewPath'              => 'backend\views\source\#table-name#',
+            'baseControllerClass'   => "backend\\controllers\\core\\BackendController",
+            'searchModelClass'      => 'backend\models\search\#TableName#Search',
+            'controllerClass'       => 'backend\controllers\source\#TableName#Controller',
+        ]
+    ];
+
+
+    
+
+
+    // php yii generate/items
+    /** генерация файлов по шаблону `/common/tpl/custom.php`
+     *
+     * @param string $items
+     */
+    public function actionItems( $items = 'all' )
+    {
+        $data   = (object) [
+            'ns'                => 'common\\models\\items',
+            'modelClass'        => '#TableName#',
+            'baseClass'         => 'common\\models\\items\\source\\#TableName#',
+        ];
+
+        $this->createCustomModel( $items, $data, 'custom' );
+    }
+}
